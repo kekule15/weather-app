d5ee8ebb15c5c32838b7a7ecbcc5044e4e187aca
Augustus <onyekachiaugustus111@gmail.com>
2025-11-06 00:05:40 +0100

    Initial Commit

---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Augustus Onyekachi on 06/11/2025.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
